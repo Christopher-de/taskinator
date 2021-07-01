7f26aba4d8360e9dd5199108c40ed459afe201c3
Get Shwifty <cjpatr0n1te@icloud.com>
2021-06-30 21:40:03 -0700

    added task with class on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
-var formEl = document.querySelector("#task-form");
-var tasksToDoEl = document.querySelector("#tasks-to-do")
+let formEl = document.querySelector("#task-form");
+let tasksToDoEl = document.querySelector("#tasks-to-do")
 
-var createTaskHandler = function(event) {
+let createTaskHandler = function(event) {
 
     event.preventDefault();
-    
-    var listItemEl = document.createElement("li");
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    let listItemEl = document.createElement("li");
     listItemEl.className = 'task-item';
-    listItemEl.textContent = 'This is a new task.';
+
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput +"</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    
     tasksToDoEl.appendChild(listItemEl);
 }
 
